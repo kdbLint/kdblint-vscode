a00d031f85a1edf011e4cfa124bc16864197e4d4
David Strachan <davidstrachan92@gmail.com>
2024-05-11 15:01:09 +0100

    Upgrade to kdbLint 0.3.0

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,23 @@
+import camelCase from "camelcase";
+import * as fs from "fs";
 import * as path from "path";
-import { ExtensionContext, window } from "vscode";
+import { ExtensionContext, commands, window, workspace } from "vscode";
 
 import {
+  CancellationToken,
+  ConfigurationParams,
+  LSPAny,
   LanguageClient,
   LanguageClientOptions,
+  RequestHandler,
+  ResponseError,
   ServerOptions,
   TransportKind,
 } from "vscode-languageclient/node";
 
 let client: LanguageClient;
+
+const debug = process.env.VSCODE_DEBUG_MODE !== undefined;
 
 const platform =
   process.platform === "win32"
@@ -41,35 +50,46 @@ export async function activate(context: ExtensionContext) {
     return;
   }
 
-  const serverCommand = context.asAbsolutePath(
-    path.join(
-      "node_modules",
-      "@kdblint",
-      "kdblint",
-      "dist",
-      platform,
-      arch,
-      "kdblint",
-    ),
-  );
+  const serverCommand = context.asAbsolutePath(getBin("kdblint"));
   const serverOptions: ServerOptions = {
     run: { command: serverCommand, transport: TransportKind.stdio },
     debug: { command: serverCommand, transport: TransportKind.stdio },
   };
+
+  const outputChannel = window.createOutputChannel("kdbLint Language Server");
 
   const clientOptions: LanguageClientOptions = {
     documentSelector: [
       { scheme: "file", language: "k" },
       { scheme: "file", language: "q" },
     ],
+    outputChannel,
+    middleware: {
+      workspace: {
+        configuration: configurationMiddleware,
+      },
+    },
   };
 
   client = new LanguageClient(
-    "kdbLintLanguageServer",
+    "kdblint",
     "kdbLint Language Server",
     serverOptions,
     clientOptions,
   );
+
+  if (debug) {
+    const debugBin = context.asAbsolutePath(getBin("kdblint.Debug"));
+    if (fs.existsSync(debugBin)) {
+      fs.renameSync(debugBin, serverCommand);
+    }
+    const interval = setInterval(() => {
+      if (fs.existsSync(debugBin)) {
+        clearInterval(interval);
+        void commands.executeCommand("workbench.action.reloadWindow");
+      }
+    }, 100);
+  }
 
   await client.start();
 }
@@ -80,4 +100,49 @@ export async function deactivate() {
   }
 
   await client.stop();
+}
+
+function getBin(bin: string): string {
+  return path.join(
+    "node_modules",
+    "@kdblint",
+    "kdblint",
+    "dist",
+    platform!,
+    arch!,
+    bin + (platform === "windows" ? ".exe" : ""),
+  );
+}
+
+async function configurationMiddleware(
+  params: ConfigurationParams,
+  token: CancellationToken,
+  next: RequestHandler<ConfigurationParams, LSPAny[], void>,
+): Promise<LSPAny[] | ResponseError> {
+  const optionIndices: Record<string, number | undefined> = {};
+
+  params.items.forEach((param, index) => {
+    if (param.section) {
+      param.section = `kdblint.${camelCase(param.section.slice("kdblint.".length))}`;
+      optionIndices[param.section] = index;
+    }
+  });
+
+  const result = await next(params, token);
+  if (result instanceof ResponseError) {
+    return result;
+  }
+
+  const configuration = workspace.getConfiguration("kdblint");
+
+  for (const name in optionIndices) {
+    const index = optionIndices[name]!;
+    const section = name.slice("kdblint.".length);
+    const configValue = configuration.get(section);
+    if (typeof configValue === "string" && configValue) {
+      result[index] = configValue;
+    }
+  }
+
+  return result as unknown[];
 }
